09305cd42dbb935d6522705824f82d933ff9fe9e
BilousOleg <olezhka82802@gmail.com>
2026-01-05 20:13:07 +0200

    Take data type into account

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,18 @@
  * @returns {boolean}
  */
 function isDivisor(inputNumber, dividend = 100) {
+  inputNumber = Number(inputNumber);
+  dividend = Number(dividend);
+
+  if (Number.isNaN(inputNumber) || Number.isNaN(dividend)) {
+    return false;
+  }
+
   return dividend % inputNumber === 0;
 }
 
 // Приклад використання
 console.log(isDivisor(10)); // У випадку, коли число є дільником
 console.log(isDivisor(9)); // У випадку, коли число не є дільником
+console.log(isDivisor('ab')); // У випадку, коли введено нечисловий тип даних (і при приведенні не отримано число)
+console.log(isDivisor('ab', 'cd')); // У випадку, коли введено нечисловий тип даних (і при приведенні не отримано число)
