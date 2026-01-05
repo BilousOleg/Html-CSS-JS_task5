c149e061aa46dd674c858cde0097a8eb0f9102a9
BilousOleg <olezhka82802@gmail.com>
2026-01-05 20:06:02 +0200

    Add fourth exercise solution

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+/**
+ * Function gets 3 sides and determines existence of tringale by their lenght
+ * Uses Number(), .isNaN()
+ * @param {number} firstSide First side of triangle
+ * @param {number} secondSide Second side of triangle
+ * @param {number} thirdSide Third side of triangle
+ * @returns {boolean}
+ */
+function isTriangle(firstSide = 3, secondSide = 4, thirdSide = 5) {
+  firstSide = Number(firstSide);
+  secondSide = Number(secondSide);
+  thirdSide = Number(thirdSide);
+
+  if (
+    Number.isNaN(firstSide) ||
+    Number.isNaN(secondSide) ||
+    Number.isNaN(thirdSide)
+  ) {
+    return false;
+  }
+
+  return (
+    firstSide + secondSide > thirdSide &&
+    firstSide + thirdSide > secondSide &&
+    secondSide + thirdSide > firstSide
+  );
+}
+
+// Приклад використання функції
+console.log(isTriangle(4, 5, 6)); // Якщо будь-яка сума двох сторін більша за третю
+console.log(isTriangle(1, 2, 3)); // Якщо хоча б одна з сум двох сторін не більша за третю
+console.log(isTriangle('', '12', null)); // Якщо введені інші типи даних (і при приведенні типів отримуємо аналог другого прикладу)
+console.log(isTriangle()); // При використанні значень за замовчуванням
