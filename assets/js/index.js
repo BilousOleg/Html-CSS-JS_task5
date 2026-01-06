fbe376a7cab66767b048daa005f0ff0f446ae0d8
BilousOleg <olezhka82802@gmail.com>
2026-01-06 21:32:28 +0200

    Change error handling method

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
 /**
+ * Funciton gets value and checks whether it is number or not
+ * @param {*} inputValue Value to check
+ * @returns {boolean}
+ */
+function isNumber(inputValue) {
+  return typeof inputValue === 'number';
+}
+
+/**
  * Function gets 3 sides and determines existence of tringale by their lenght
  * Uses Number(), .isNaN()
  * @param {number} [firstSide = 3] First side of triangle
@@ -7,27 +16,19 @@
  * @returns {boolean}
  */
 function isTriangle(firstSide = 3, secondSide = 4, thirdSide = 5) {
-  firstSide = Number(firstSide);
-  secondSide = Number(secondSide);
-  thirdSide = Number(thirdSide);
-
-  if (
-    Number.isNaN(firstSide) ||
-    Number.isNaN(secondSide) ||
-    Number.isNaN(thirdSide)
-  ) {
+  if (isNumber(firstSide) && isNumber(secondSide) && isNumber(thirdSide)) {
+    return (
+      firstSide + secondSide > thirdSide &&
+      firstSide + thirdSide > secondSide &&
+      secondSide + thirdSide > firstSide
+    );
+  } else {
     return false;
   }
-
-  return (
-    firstSide + secondSide > thirdSide &&
-    firstSide + thirdSide > secondSide &&
-    secondSide + thirdSide > firstSide
-  );
 }
 
 // Приклад використання функції
 console.log(isTriangle(4, 5, 6)); // Якщо будь-яка сума двох сторін більша за третю
 console.log(isTriangle(1, 2, 3)); // Якщо хоча б одна з сум двох сторін не більша за третю
-console.log(isTriangle('', '12', null)); // Якщо введені інші типи даних (і при приведенні типів отримуємо аналог другого прикладу)
+console.log(isTriangle('', '12', null)); // Якщо введені інші типи даних
 console.log(isTriangle()); // При використанні значень за замовчуванням
