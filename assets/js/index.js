03868e416069a642b42bedd79674d5a9f90dc7fb
BilousOleg <olezhka82802@gmail.com>
2026-01-06 21:08:33 +0200

    Change error handling method

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,27 @@
 /**
+ * Funciton gets value and checks whether it is number or not
+ * @param {*} inputValue Value to check
+ * @returns {boolean}
+ */
+function isNumber(inputValue) {
+  return typeof inputValue === 'number';
+}
+
+/**
  * Function gets a number and checks whether it is a divisor of divident
  * @param {number} inputNumber Number to check
  * @param {number} [dividend = 100] Divident (optional parameter, default value = 100)
  * @returns {boolean}
  */
 function isDivisor(inputNumber, dividend = 100) {
-  inputNumber = Number(inputNumber);
-  dividend = Number(dividend);
-
-  if (Number.isNaN(inputNumber) || Number.isNaN(dividend)) {
+  if (isNumber(inputNumber) === false || isNumber(dividend) === false) {
     return false;
   }
-
   return dividend % inputNumber === 0;
 }
 
 // Приклад використання
 console.log(isDivisor(10)); // У випадку, коли число є дільником
 console.log(isDivisor(9)); // У випадку, коли число не є дільником
-console.log(isDivisor('ab')); // У випадку, коли введено нечисловий тип даних (і при приведенні не отримано число)
-console.log(isDivisor('ab', 'cd')); // У випадку, коли введено нечисловий тип даних (і при приведенні не отримано число)
+console.log(isDivisor('ab')); // У випадку, коли введено нечисловий тип даних
+console.log(isDivisor(1, 'cd')); // У випадку, коли введено нечисловий тип даних у якості необов'язкового аргументу
